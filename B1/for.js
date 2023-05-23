3e6bb91dd97296c3c2c261d6d1168083611f5460
Trung Phan <hieutrunqphan@gmail.com>
2023-05-23 14:10:54 +0700

    UngDungMayTinh

---
+++ B1/for.js
@@ -0,0 +1,33 @@
+// let number = 10;
+// let total = 0;
+// for (let i = 1; i <= 10; i++) {
+//     total = total + i;
+// }
+
+// console.log(total);
+
+// let n = 5;
+// if (n++ == 5) {
+//     console.log(true, n)
+// }
+// else {
+//     console.log(false, n)
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i)
+// }
+
+
+// let sum = 0;
+// for (let i = 1; i <= 20; i++) {
+//     sum = sum + i;
+//     console.log("tong tien " + sum + " tai ngay thu " + i);
+// }
+// console.log(sum)
+//num%2
+
+
+for (let i = 5; i <= 20; i++) {
+    console.log(i)
+}
